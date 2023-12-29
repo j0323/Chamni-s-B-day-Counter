25f36a12f3ac17399614354ddea91c10ebb0dc72
j0323 <121381226+j0323@users.noreply.github.com>
2023-12-29 23:42:54 +0530

    Changed the Birthday

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 03, 2023',
+  birthdate: 'Jan 03, 2024',
   name: 'Chamni'
 };
 
